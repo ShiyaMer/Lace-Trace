2a6d7857efec287e4c834e0a894d4bde61835afe
ShiyaMer <95877906+ShiyaMer@users.noreply.github.com>
2024-08-13 01:14:10 +0530

    Update main.py

--- Langchain Chatbot/main.py
+++ Langchain Chatbot/main.py
@@ -86,7 +86,7 @@ def process_input():
 
 def main():
     system_prompt = 'You are a helpful assistant specializing in shoe authentication.'
-    groq_api_key = "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"
+    groq_api_key = os.environ.get("GROQ_API_KEY")
     model = 'llama3-8b-8192' 
     conversational_memory_length = 16
     memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)   
